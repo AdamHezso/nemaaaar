26897881002dd54db3af4543750b15640aa97926
Hézső Károly Ádám <hezso.adm19k@egressy.info>
2024-12-03 14:03:15 +0100

    Add project files.

---
+++ nemaaaar/klassz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace nemaaaar
+{
+  
+    
+        public class KecskeClass
+        {
+            public int id { get; set; }
+            public int kor { get; set; }
+            public int suly { get; set; }
+            public int magassag { get; set; }
+            public string nem { get; set; }
+            public string nev { get; set; }
+        }
+    
+
+}
+
